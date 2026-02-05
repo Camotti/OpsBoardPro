4596276c1ca3f25b473284a55c1e6dd47297dcdc
Camilo Rodriguez <camiropi97@gmail.com>
2026-02-04 22:52:12 -0500

    routes and app shell are configured

--- opsboard-front/src/app/app.routes.ts
+++ opsboard-front/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AppShellComponent } from './layouts/app-shell/app-shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: AppShellComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full',
+            },
+
+            {
+                path: 'dashboard',
+                loadChildren: ()=> 
+                    import('./features/dashboard/pages/dashboard.routes').then( 
+                        (m) => m.DASHBOARD_ROUTES
+                    ),
+            },
+        ],
+    },
+];
